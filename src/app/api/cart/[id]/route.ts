78c314089e99f7303d6e3e5b4ca7efe6e2ec1090
evkondr <kondratiev.evg@gmail.com>
2025-12-14 23:25:15 +0300

    feat: add PATCH route for cart

---
+++ src/app/api/cart/[id]/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma-client";
+import { updateCartTotalAmount } from "@/lib/update-cart-total-amount";
+import { NextRequest, NextResponse } from "next/server";
+interface Params {
+  params: Promise<{ id: string }>
+}
+export async function PATCH(req:NextRequest, { params }:Params) {
+  try {
+    const { id } = await params
+    //Getting data from request body
+    const data = (await req.json()) as { quantity: number}
+    //Getting cart token from cookie
+    const cartToken = req.cookies.get('cartToken')?.value;
+    if (!cartToken) {
+      return NextResponse.json({ error: 'Токен не найден'})
+    };
+    const cartItem = await prisma.cartItem.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!cartItem) {
+      return NextResponse.json({ error: 'Продукт в корзине не найден' });
+    }
+    await prisma.cartItem.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        quantity: data.quantity
+      }
+    })
+    const updatedUserCart = await updateCartTotalAmount(cartToken);
+
+    return NextResponse.json(updatedUserCart);
+  } catch (error) {
+    console.log('[CART_PATCH] Server error', error);
+    return NextResponse.json({ message: 'Не удалось обновить корзину ' }, { status: 500 });
+  }
+
+} 
---
+++ src/lib/update-cart-total-amount.ts
@@ -0,0 +1,53 @@
+import { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
+import { prisma } from "./prisma-client";
+
+//Reusable function for updating cart total amount in several API routes
+export const updateCartTotalAmount = async (token: string) => {
+  const userCart = await prisma.cart.findFirst({
+    where: {
+      token,
+    },
+    include: {
+      items: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          productItem: {
+            include: {
+              product: true,
+            },
+          },
+          ingredients: true,
+        },
+      },
+    },
+  });
+  if(!userCart) return;
+   const totalAmount = userCart.items.reduce((acc, item) => {
+    return acc + calcCartItemTotalPrice(item);
+  }, 0);
+  return await prisma.cart.update({
+    where: {
+      id: userCart.id,
+    },
+    data: {
+      totalAmount,
+    },
+    include: {
+      items: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          productItem: {
+            include: {
+              product: true,
+            },
+          },
+          ingredients: true,
+        },
+      },
+    },
+  });
+}
