311ca1a2cf7d904fac76665e111132eb9700b248
evkondr <kondratiev.evg@gmail.com>
2025-12-24 23:47:59 +0300

    feat: add sections WhiteBlock in CheckoutPage

--- src/app/(checkout)/checkout/page.tsx
+++ src/app/(checkout)/checkout/page.tsx
@@ -1,5 +1,32 @@
-export default function Checkout() {
+import { Container, Title } from "@/shared";
+import { WhiteBlock } from "@/shared/WhiteBlock";
+
+export default function CheckoutPage() {
   return (
-    <div>Checkout Page</div>
+    <Container className="mt-10">
+      <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
+      <div className="flex gap-10">
+        <div className="flex flex-col gap-10 flex-1 mb-20">
+          <WhiteBlock title="1. Персональные данные"></WhiteBlock>
+          <WhiteBlock title="2. Персональные данные">
+            <div className="grid grid-cols-2 gap-5">
+              <input name="firstName" className="text-base" placeholder="Имя" />
+              <input name="lastName" className="text-base" placeholder="Фамилия" />
+              <input name="email" className="text-base" placeholder="E-Mail" />
+              <input name="phone" className="text-base" placeholder="Телефон" />
+            </div>
+          </WhiteBlock>
+          <WhiteBlock title="3. Адрес доставки">
+            <div className="flex flex-col gap-5">
+              <input name="firstName" className="text-base" placeholder="Имя" />
+              <textarea name="comment" className="text-base" placeholder="Комментарий к заказу" rows={5}/>
+            </div>
+          </WhiteBlock>
+        </div>
+        <div className="w-[450px]">
+          sidebar
+        </div>
+      </div>
+    </Container>
   );
 }
