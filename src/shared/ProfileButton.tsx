42cae78567ada3180a1632b107769b14d47c59f9
evkondr <kondratiev.evg@gmail.com>
2026-01-19 23:53:39 +0300

    refactor: ProfileButton

--- src/shared/ProfileButton.tsx
+++ src/shared/ProfileButton.tsx
@@ -1,13 +1,32 @@
-import { User } from 'lucide-react'
+import { CircleUser, User } from 'lucide-react'
 import React from 'react'
 import { Button } from '../components/ui/button'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { PropsWithClass } from '@/types'
 
-const ProfileButton = () => {
+interface Props extends PropsWithClass {
+  onClickSignIn?: () => void;
+}
+
+const ProfileButton = ({
+  onClickSignIn,
+  className
+}:Props) => {
+  const { data:session } = useSession();
   return (
-    <Button variant="outline" className="flex items-center gap-1 cursor-pointer">
-      <User size={16} />
-      Войти
-    </Button>
+    <div className={className}>
+      {!session ? (
+      <Button onClick={onClickSignIn} variant="outline" className="flex items-center gap-1 cursor-pointer">
+        <User size={16} />
+        Войти
+      </Button>) : (<Link href='/profile'>
+      <Button variant="secondary" className="flex items-center gap-2">
+        <CircleUser size={16} />
+        Профиль
+      </Button>
+    </Link>)}
+    </div>
   )
 }
 
