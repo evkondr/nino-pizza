22ae1a34e1c139b78561618edfd9ecb0fe4f97de
evkondr <kondratiev.evg@gmail.com>
2026-02-20 19:48:04 +0300

    fix: add suspense

--- src/app/(checkout)/layout.tsx
+++ src/app/(checkout)/layout.tsx
@@ -1,10 +1,13 @@
 import { Container, Header } from "@/shared";
+import { Suspense } from "react";
 
 export default function CheckoutLayout({children}: {children: React.ReactNode}) {
   return (
     <main className="min-h-screen bg-[#F4F1EE]">
       <Container>
-        <Header hasSearch={false} hasCart={false}  className="border-gray-200" />
+        <Suspense>
+          <Header hasSearch={false} hasCart={false}  className="border-gray-200" />
+        </Suspense>
         {children}
       </Container>
     </main>
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import Header from "@/shared/Header";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "NiNo Pizza",
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <main className="min-h-screen">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       {children}
       {modal}
     </main>
