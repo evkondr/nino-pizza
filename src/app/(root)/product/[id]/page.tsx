3ec46e42a8788bf6844f039b07893ef2ce843dbb
evkondr <kondratiev.evg@gmail.com>
2026-02-21 11:52:45 +0300

    feat: add meta in product

--- src/app/(root)/product/[id]/page.tsx
+++ src/app/(root)/product/[id]/page.tsx
@@ -1,10 +1,29 @@
 import { prisma } from "@/lib/prisma-client";
 import { Container } from "@/shared";
 import ProductForm from "@/shared/ProductForm";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: Promise<{ id: string }>
+}
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+  const {id} = await params;
+  const product = await prisma.product.findFirst({
+    where: { 
+      id: Number(id)
+    },
+    include: {
+      items: true,
+      ingredients: true,
+    }
+  });
+  return {
+    title: product?.name,
+    description: product?.ingredients.map((ingredient) => ingredient.name).join(', '),
+  };
 }
 export default async function Product({ params }:Props) {
   const {id} = await params;
